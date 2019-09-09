5ee38cf1f7610c4fbc9dfef4f969350f99b640ed
zhanglingya <897226478@qq.com>
2019-09-09 16:29:02 +0800

    search
    search

---
+++ search/search.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function getKey(){
+  if(event.keyCode==13){
+    var text =document.getElementById("text").value;
+    document.getElementById("check").value =text;
+  }
+};
+function del(){
+  document.getElementById("check").value = "";
+};
