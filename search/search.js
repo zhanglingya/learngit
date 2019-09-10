95351b079efd51c1edc13a02454c130e96a71038
zhanglingya <897226478@qq.com>
2019-09-10 11:14:07 +0800

    search
    search

--- search/search.js
+++ search/search.js
@@ -1,14 +1,19 @@
 'use strict';
 
-function getKey(){
+function getKey(){ 
   if(event.keyCode==13){
     var text =document.getElementById("text").value;
-    document.getElementById("check").value =text;
-  }
-};
+    var node = document.createElement("li");
+    var list = document.createTextNode(text);
+    node.appendChild(list);
+    document.getElementById("list").appendChild(node);
+    document.getElementById("text").value="";
+    
+  }  
+}
 function del(){
-  document.getElementById("check").value = "";
+  var text =document.getElementById("text").value;
+    var node = document.createElement("li");
+    node.innerHTML='<a href="javascript:;">删除</a>'
+    node.appendChild(list);
 };
-$('dropdown').each(function(){
-  this.style='display:none'
-});
