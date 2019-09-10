0b2d4b8a3157875f987b21db97ddc9842f824581
zhanglingya <897226478@qq.com>
2019-09-10 14:22:25 +0800

    search
    search

--- search/search.js
+++ search/search.js
@@ -1,19 +1,21 @@
-'use strict';
+"use strict";
 
-function getKey(){ 
-  if(event.keyCode==13){
-    var text =document.getElementById("text").value;
-    var node = document.createElement("li");
-    var list = document.createTextNode(text);
-    node.appendChild(list);
-    document.getElementById("list").appendChild(node);
+function getKey() {
+  if (event.keyCode == 13) {
+    var otab = document.getElementById("list");
+    var text = document.getElementById("text");
+    var otr = document.createElement("tr");
+    var otd = document.createElement("td");
+    otd.innerHTML = text.value;
+    otr.appendChild(otd);
     document.getElementById("text").value="";
-    
-  }  
+    var otd = document.createElement("td");
+    otd.innerHTML='<a href="#">×</a>';
+    otr.appendChild(otd);
+    otd.getElementsByTagName('a')[0].onclick=function(){
+      otab.tBodies[0].removeChild(this.parentNode.parentNode);
+  }
+  
+  otab.tBodies[0].appendChild(otr);
+  }
 }
-function del(){
-  var text =document.getElementById("text").value;
-    var node = document.createElement("li");
-    node.innerHTML='<a href="javascript:;">删除</a>'
-    node.appendChild(list);
-};
