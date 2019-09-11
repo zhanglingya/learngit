7ca2e57dd9b02e1be1e5ac0e09da692e90f0a6cf
zhanglingya <897226478@qq.com>
2019-09-11 09:54:36 +0800

    search
    search

--- search/index.js
+++ search/index.js
@@ -1,19 +1,24 @@
 "use strict";
 var text = document.getElementById('text');
+
 text.addEventListener("keydown",function getKey(){
   if (event.keyCode === 13) {
     var table = document.getElementById('list');
+
     var line = document.createElement('tr');
     var row = document.createElement('td');
     row.innerHTML = text.value;
     line.appendChild(row);
+
     var rank = document.createElement('td');
     rank.innerHTML = '<a href="#">×</a>';
     line.appendChild(rank);
+
     rank.getElementsByTagName('a')[0].onclick = function() {
       table.tBodies[0].removeChild(this.parentNode.parentNode);
     };
     table.tBodies[0].appendChild(line);
+
     document.getElementById('text').value = '';
   }
 })
