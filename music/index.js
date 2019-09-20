e8e4f8b3a741c90433553a2b577055e1fc38888c
zhanglingya <897226478@qq.com>
2019-09-20 11:10:33 +0800

    music
    music

--- music/index.js
+++ music/index.js
@@ -1,97 +1,96 @@
-"use strict";
-var icon = document.getElementById("icon");
-var song = document.getElementById("song");
-var cd = document.getElementById("cd");
-var circle = document.getElementById("circle");
-var goodnight = document.getElementById("goodnight");
-var menu = document.getElementById("menu");
-var progress = document.getElementById("progress");
-var loader = document.getElementById("loader");
+'use strict';
+var icon = document.getElementById('icon');
+var song = document.getElementById('song');
+var cd = document.getElementById('cd');
+var circle = document.getElementById('circle');
+var goodnight = document.getElementById('goodnight');
+var menu = document.getElementById('menu');
+var progress = document.getElementById('progress');
+var loader = document.getElementById('loader');
 var music = new Audio();
-music.src = "asset/颜人中 - 晚安.mp3";
+music.src = 'asset/颜人中 - 晚安.mp3';
 var interval;
 var button;
 var key;
 
 //播放
-icon.addEventListener("click", function() {
+icon.addEventListener('click', function() {
   //点击播放按钮播放音乐
   music.play();
 
   //点击播放按钮弹出歌曲信息
-  song.style.transition = "all 1.5s";
-  song.style.top = "130px";
+  song.style.transition = 'all 1.5s';
+  song.style.top = '130px';
 
   //弹出暂停按钮，隐藏播放按钮
-  icon.style.display = "none";
+  icon.style.display = 'none';
   button = setInterval(() => {
-    menu.style.display = "block";
-    menu.style.left = "-10px";
+    menu.style.display = 'block';
+    menu.style.left = '-10px';
   }, 200);
   clearInterval(key);
 
   //点击播放按钮图片变大
-  cd.style.transition = "all 0.8s";
-  circle.style.transition = "all 0.6s";
-  cd.style.height = "90px";
-  cd.style.width = "90px";
-  circle.style.height = "20px";
-  circle.style.width = "20px";
-  circle.style.top = "15px";
-  circle.style.left = "55px";
+  cd.style.transition = 'all 0.8s';
+  circle.style.transition = 'all 0.6s';
+  cd.style.height = '90px';
+  cd.style.width = '90px';
+  circle.style.height = '20px';
+  circle.style.width = '20px';
+  circle.style.top = '15px';
+  circle.style.left = '55px';
 
   //点击播放按钮图片一直旋转
   var rotate = 0;
   interval = setInterval(function() {
     rotate += 1;
-    cd.style.transform = "rotate(" + rotate + "deg)";
-    cd.style.transition = "0.2s linear";
+    cd.style.transform = 'rotate(' + rotate + 'deg)';
+    cd.style.transition = '0.2s linear';
   }, 100);
 
   //进度条
-  music.addEventListener("timeupdate", show, false);
+  music.addEventListener('timeupdate', show, false);
 
   //显示进度条
   function show() {
     var value = (music.currentTime / music.duration) * 100;
-    loader.style.width = value + "%";
+    loader.style.width = value + '%';
   }
 
   //拖动进度条
-  progress.addEventListener("click", function(e) {
-    var rate =
-      (e.offsetX - (progress.width() - loader.width()) / 2) / loader.width();
+  progress.addEventListener('click', function(e) {
+    var rate = e.offsetX / 225;
     music.currentTime = music.duration * rate;
     show();
   });
 });
 
 //暂停
-menu.addEventListener("click", function() {
+menu.addEventListener('click', function() {
   //缩回歌曲信息
-  song.style.transition = "all 1s";
-  song.style.top = "200px";
-  song.style.visibility = "0";
+  song.style.transition = 'all 1s';
+  song.style.top = '200px';
+  song.style.visibility = '0';
 
   //弹出播放按钮，隐藏暂停按钮
-  menu.style.display = "none";
+  menu.style.display = 'none';
   key = setInterval(() => {
-    icon.style.display = "block";
+    icon.style.display = 'block';
   }, 200);
   clearInterval(button);
 
   //恢复图片原始状态
-  cd.style.transition = "all 0.8s";
-  circle.style.transition = "all 0.6s";
-  cd.style.height = "75px";
-  cd.style.width = "75px";
-  circle.style.height = "15px";
-  circle.style.width = "15px";
-  circle.style.top = "10px";
-  circle.style.left = "50px";
+  cd.style.transition = 'all 0.8s';
+  circle.style.transition = 'all 0.6s';
+  cd.style.height = '75px';
+  cd.style.width = '75px';
+  circle.style.height = '15px';
+  circle.style.width = '15px';
+  circle.style.top = '10px';
+  circle.style.left = '50px';
 
   //清除定时器
-  cd.style.transform = "rotate(0deg)";
+  cd.style.transform = 'rotate(0deg)';
   clearInterval(interval);
 
   //暂停音乐
